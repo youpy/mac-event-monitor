00cf4d18dae399251a5c438fde06e94613f2edb1
youpy <youpy@buycheapviagraonlinenow.com>
2012-04-14 16:33:16 +0900

    initial commit

---
+++ lib/mac-event-monitor.rb
@@ -0,0 +1,9 @@
+require "mac-event-monitor/version"
+
+module Mac
+  module Event
+    module Monitor
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/mac-event-monitor/version.rb
@@ -0,0 +1,7 @@
+module Mac
+  module Event
+    module Monitor
+      VERSION = "0.0.1"
+    end
+  end
+end
